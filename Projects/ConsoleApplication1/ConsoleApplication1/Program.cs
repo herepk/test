3ea6d625da11a2a6b5bc4a1818bba2260d68a62f
Kumar <pkum123@ms.ds.uhc.com>
2017-12-11 18:13:00 +0530

    Added new Project in test dir

---
+++ Projects/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld    
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CreateMessage());
+        } 
+        public static String CreateMessage()
+        {
+            return "Hello World";
+        }
+    }
+}
